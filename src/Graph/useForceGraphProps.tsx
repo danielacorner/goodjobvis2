7691e3cf043f5553aeed70a4fc59f07fcb2834ef
daniel.corner <danielcorner7@gmail.com>
2021-09-28 23:33:06 -0400

    custom node object

--- src/Graph/useForceGraphProps.tsx
+++ src/Graph/useForceGraphProps.tsx
@@ -1,17 +1,15 @@
-import * as THREE from "three";
-import * as d3 from "d3";
 import { useWindowSize } from "../hooks/useWindowSize";
 import { useCallback, useRef } from "react";
 import { useControls } from "leva";
 
+type GraphNode = { id_str: string };
+
 export const NODE_DIAMETER = 25;
 export const AVATAR_DIAMETER = NODE_DIAMETER * 4;
 const LIGHTGREY = "#D3D3D3";
-const DARKGREY = "#a9a9a9";
 const LIKE = "#a40880";
 const RETWEET_TO_TWEET = "#179245";
 const USER_TO_TWEET = LIGHTGREY;
-const DEFAULT_NODE_COLOR = "steelblue";
 /** https://www.npmjs.com/package/react-force-graph */
 
 // tslint:disable-next-line
@@ -20,6 +18,7 @@ export function useForceGraphProps() {
   const onBackgroundClick = useCallback(() => {
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
+  const tooltipNode = { id_str: "123" };
   const { isLikeLink, isRetweetLink, isTweetToRetweetLink, other } =
     useControls({
       isLikeLink: false,
@@ -62,87 +61,98 @@ export function useForceGraphProps() {
     // nodeColor: (node) => getNodeColor(node, colorBy),
     // onEngineStop: () =>
     //   fgRef.current && !is3d ? (fgRef.current as any).zoomToFit(400) : null,
-    // nodeCanvasObject: ((node: Tweet & { x: number; y: number }, ctx) => {
-    //   // draw the bot score if we have one
-    //   const MOCK_BOT_SCORE: BotScore = {
-    //     overall: 1,
-    //     astroturf: 2,
-    //     fake_follower: 3,
-    //     financial: 4,
-    //     self_declared: 5,
-    //     spammer: 4,
-    //     other: 5,
-    //   };
-    //   if (MOCK_BOT_SCORE || node.botScore) {
-    //     // if (node.isUserNode && node.botScore) {
-    //     drawBotScore({ ...node, botScore: MOCK_BOT_SCORE }, ctx);
-    //   }
-    //   if (node.sentimentResult) {
-    //     // if (node.isUserNode && node.botScore) {
-    //     drawSentimentResult(node, ctx);
-    //   }
+    nodeCanvasObject: ((node: GraphNode & { x: number; y: number }, ctx) => {
+      //   // draw the bot score if we have one
+      //   const MOCK_BOT_SCORE: BotScore = {
+      //     overall: 1,
+      //     astroturf: 2,
+      //     fake_follower: 3,
+      //     financial: 4,
+      //     self_declared: 5,
+      //     spammer: 4,
+      //     other: 5,
+      //   };
+      //   if (MOCK_BOT_SCORE || node.botScore) {
+      //     // if (node.isUserNode && node.botScore) {
+      //     drawBotScore({ ...node, botScore: MOCK_BOT_SCORE }, ctx);
+      //   }
+      //   if (node.sentimentResult) {
+      //     // if (node.isUserNode && node.botScore) {
+      //     drawSentimentResult(node, ctx);
+      //   }
 
-    //   if (tooltipNode?.id_str === node.id_str) {
-    //     drawHighlightCircle(node, ctx, Boolean(tooltipNode?.isUserNode));
-    //   }
+      if (tooltipNode?.id_str === node.id_str) {
+        drawHighlightCircle(node, ctx);
+      }
+      ctx.beginPath();
+      ctx.arc(
+        node.x, // x: The horizontal coordinate of the arc's center.
+        node.y, // y: The vertical coordinate of the arc's center.
+        AVATAR_DIAMETER / 2 - 10, // radius
+        0, // startAngle
+        Math.PI * 2 // endAngle
+      );
+      ctx.fillStyle = "tomato";
+      ctx.fill();
+      ctx.closePath();
 
-    //   if (colorBy === COLOR_BY.profilePhoto || node.isUserNode) {
-    //     if (isStorybook) {
-    //       // draw offline indicatr
-    //       ctx.beginPath();
-    //       ctx.arc(
-    //         node.x, // x: The horizontal coordinate of the arc's center.
-    //         node.y, // y: The vertical coordinate of the arc's center.
-    //         AVATAR_DIAMETER / 2 - 10, // radius
-    //         0, // startAngle
-    //         Math.PI * 2 // endAngle
-    //       );
-    //       ctx.fillStyle = "tomato";
-    //       ctx.fill();
-    //     } else {
-    //       // show profile photo
-    //       drawProfilePhoto(node, ctx);
-    //     }
-    //   } else if (colorBy === COLOR_BY.media) {
-    //     // show media
-    //     const mediaArr = getMediaArr(node);
-    //     if (!mediaArr[0]) {
-    //       // draw an empty circle if there's no media
-    //       ctx.beginPath();
-    //       ctx.arc(node.x, node.y, NODE_DIAMETER / 2, 0, Math.PI * 2);
-    //       // stroke styles https://developer.mozilla.org/en-US/docs/Web/API/CanvasRenderingContext2D/stroke
-    //       ctx.strokeStyle = "cornflowerblue";
-    //       ctx.stroke();
-    //     } else {
-    //       const image = mediaArr[0];
-    //       const small = image.sizes.small;
-    //       const hwRatio = small.h / small.w;
-    //       const imgHeight = NODE_DIAMETER * hwRatio * 2;
-    //       const imgWidth = NODE_DIAMETER * 2;
+      //   if (colorBy === COLOR_BY.profilePhoto || node.isUserNode) {
+      //     if (isStorybook) {
+      //       // draw offline indicatr
+      //       ctx.beginPath();
+      //       ctx.arc(
+      //         node.x, // x: The horizontal coordinate of the arc's center.
+      //         node.y, // y: The vertical coordinate of the arc's center.
+      //         AVATAR_DIAMETER / 2 - 10, // radius
+      //         0, // startAngle
+      //         Math.PI * 2 // endAngle
+      //       );
+      //       ctx.fillStyle = "tomato";
+      //       ctx.fill();
+      //     } else {
+      //       // show profile photo
+      //       drawProfilePhoto(node, ctx);
+      //     }
+      //   } else if (colorBy === COLOR_BY.media) {
+      //     // show media
+      //     const mediaArr = getMediaArr(node);
+      //     if (!mediaArr[0]) {
+      //       // draw an empty circle if there's no media
+      //       ctx.beginPath();
+      //       ctx.arc(node.x, node.y, NODE_DIAMETER / 2, 0, Math.PI * 2);
+      //       // stroke styles https://developer.mozilla.org/en-US/docs/Web/API/CanvasRenderingContext2D/stroke
+      //       ctx.strokeStyle = "cornflowerblue";
+      //       ctx.stroke();
+      //     } else {
+      //       const image = mediaArr[0];
+      //       const small = image.sizes.small;
+      //       const hwRatio = small.h / small.w;
+      //       const imgHeight = NODE_DIAMETER * hwRatio * 2;
+      //       const imgWidth = NODE_DIAMETER * 2;
 
-    //       // show the first image/video preview
+      //       // show the first image/video preview
 
-    //       const ctxImg = new Image(imgWidth, imgHeight);
-    //       ctxImg.src = image.poster || image.src;
+      //       const ctxImg = new Image(imgWidth, imgHeight);
+      //       ctxImg.src = image.poster || image.src;
 
-    //       // drawImage https://developer.mozilla.org/en-US/docs/Web/API/CanvasRenderingContext2D/drawImage
-    //       ctx.drawImage(
-    //         ctxImg,
-    //         node.x - imgWidth / 2,
-    //         node.y - imgHeight / 2,
-    //         imgWidth,
-    //         imgHeight
-    //       );
-    //     }
-    //   } else {
-    //     // draw circle
-    //     ctx.beginPath();
-    //     ctx.arc(node.x, node.y, NODE_DIAMETER / 2, 0, Math.PI * 2);
-    //     // stroke styles https://developer.mozilla.org/en-US/docs/Web/API/CanvasRenderingContext2D/stroke
-    //     ctx.fillStyle = getNodeColor(node, colorBy);
-    //     ctx.fill();
-    //   }
-    // }) as any,
+      //       // drawImage https://developer.mozilla.org/en-US/docs/Web/API/CanvasRenderingContext2D/drawImage
+      //       ctx.drawImage(
+      //         ctxImg,
+      //         node.x - imgWidth / 2,
+      //         node.y - imgHeight / 2,
+      //         imgWidth,
+      //         imgHeight
+      //       );
+      //     }
+      //   } else {
+      //     // draw circle
+      //     ctx.beginPath();
+      //     ctx.arc(node.x, node.y, NODE_DIAMETER / 2, 0, Math.PI * 2);
+      //     // stroke styles https://developer.mozilla.org/en-US/docs/Web/API/CanvasRenderingContext2D/stroke
+      //     ctx.fillStyle = getNodeColor(node, colorBy);
+      //     ctx.fill();
+      //   }
+    }) as any,
     linkWidth: 1,
     backgroundColor: "transparent",
     linkColor: (({ source, target }) => {
@@ -270,14 +280,14 @@ export function useForceGraphProps() {
   return { fgRef, forceGraphProps };
 }
 
-function drawHighlightCircle(node: any, ctx: any, isUserNode: boolean) {
+function drawHighlightCircle(node: any, ctx: any) {
   // circle
   ctx.beginPath();
   // https://developer.mozilla.org/en-US/docs/Web/API/CanvasRenderingContext2D/arc
   ctx.arc(
     node.x, // x: The horizontal coordinate of the arc's center.
     node.y, // y: The vertical coordinate of the arc's center.
-    isUserNode ? AVATAR_DIAMETER : AVATAR_DIAMETER / 2, // radius
+    AVATAR_DIAMETER, // radius
     0, // startAngle
     Math.PI * 2 // endAngle
   );
