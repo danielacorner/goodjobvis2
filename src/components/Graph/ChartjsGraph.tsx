5a4aa2a0f2f66b25d0d3f42f1a8aaa98203ba95f
daniel.corner <danielcorner7@gmail.com>
2022-05-27 23:41:33 -0400

    chartjs touchups

--- src/components/Graph/ChartjsGraph.tsx
+++ src/components/Graph/ChartjsGraph.tsx
@@ -1,3 +1,4 @@
+import styled from "styled-components/macro";
 import {
   Chart as ChartJS,
   LinearScale,
@@ -8,31 +9,45 @@ import {
 } from "chart.js";
 import { Scatter } from "react-chartjs-2";
 import { NOC_NODES } from "../../assets/NOC-node";
+import { useWindowSize } from "../../hooks/useWindowSize";
 
 // https://react-chartjs-2.netlify.app/examples/scatter-chart
 ChartJS.register(LinearScale, PointElement, LineElement, Tooltip, Legend);
 
-export const options = {
-  scales: {
-    y: {
-      beginAtZero: true,
-    },
-  },
-};
-
-export const data = {
-  datasets: [
-    {
-      label: "A dataset",
-      data: NOC_NODES.map((node) => ({
-        x: Math.random() * 100,
-        y: Math.random() * 100,
-      })),
-      backgroundColor: "rgba(255, 99, 132, 1)",
-    },
-  ],
-};
-
 export function ChartjsGraph() {
-  return <Scatter options={options} data={data} />;
+  const { width, height } = useWindowSize();
+  return (
+    <ChartStyles>
+      <Scatter
+        height={height}
+        width={width}
+        options={{
+          scales: {
+            y: {
+              beginAtZero: true,
+            },
+          },
+          layout: {
+            padding: 50,
+          },
+        }}
+        data={{
+          datasets: [
+            {
+              label: "A dataset",
+              data: NOC_NODES.map((node) => ({
+                x: Math.random() * 100,
+                y: Math.random() * 100,
+              })),
+              backgroundColor: "rgba(255, 99, 132, 1)",
+            },
+          ],
+        }}
+      />
+    </ChartStyles>
+  );
 }
+const ChartStyles = styled.div`
+  position: fixed;
+  inset: 0;
+`;
